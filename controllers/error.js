245c5656ebf8d398a3441b218a71624648b3c816
Yegor Tsukanov <yehor.tsukanov@gmail.com>
2021-02-16 12:04:29 +0200

    modified

--- controllers/error.js
+++ controllers/error.js
@@ -1,6 +1,4 @@
-exports.get404 = (req, res, next)=>{
-    res.status(404).render('404', {
-        pageTitle: 'Page not found',
-        path: '/404'
-    });
-};
+exports.get404 = (req, res, next) => {
+    res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/404' });
+  };
+  
--- routes/shop.js
+++ routes/shop.js
@@ -14,8 +14,12 @@ router.get('/products/:productId', shopController.getProduct);
 
 router.get('/cart', shopController.getCart);
 
+router.post('/cart', shopController.postCart);
+
+router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+
 router.get('/orders', shopController.getOrders);
 
-router.get('/checkout', shopController.getCheckout);    
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
